6e43d2874587ca257df01282d2a531c5ef84d35b
DawidTwardy <150292727+DawidTwardy@users.noreply.github.com>
2025-12-11 03:58:42 +0100

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,41 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // W Server-Side Components (SSC) i Serverless Functions (API Routes)
-    // te moduły powinny działać. Musimy je wykluczyć z budowania po stronie klienta.
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        net: false,
-        dns: false,
-        tls: false,
-        module: false,
-        path: false,
-        stream: false,
-        buffer: false,
-        util: false,
-        crypto: false,
-        url: false,
-        os: false,
-        assert: false,
-        http: false,
-        https: false,
-        zlib: false,
-        querystring: false,
-        tty: false,
-        constants: false,
-        string_decoder: false,
-        events: false,
-        child_process: false,
-        vm: false,
-        'async_hooks': false,
-        'diagnostics_channel': false,
-        'worker_threads': false,
-      };
-    }
-
-    return config;
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
-export default nextConfig;
+export default config;

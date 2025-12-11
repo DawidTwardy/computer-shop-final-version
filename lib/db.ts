9bbf058ca5c367356fb1cb6d41153cad801794a0
DawidTwardy <150292727+DawidTwardy@users.noreply.github.com>
2025-12-11 04:09:52 +0100

    Clarify PG adapter usage and singleton pattern in db.ts
    Refactor db.ts to clarify usage of PG adapter and singleton pattern.

--- lib/db.ts
+++ lib/db.ts
@@ -9,15 +9,12 @@ if (!connectionString) {
   throw new Error("Brak DATABASE_URL w zmiennych środowiskowych")
 }
 
-// Używamy globalnego obiektu do singletona, ale z adapterem
-// W trybie produkcji (Vercel) Adapter musi zostać załadowany
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-// Jeśli to nie produkcja, używamy istniejącego globalnego, aby uniknąć ponownego tworzenia połączenia.
-// W produkcji tworzymy nową instancję.
+// Użycie adaptera PG dla Prisma i Patternu Singletona (kluczowe dla uniknięcia wielu połączeń)
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma

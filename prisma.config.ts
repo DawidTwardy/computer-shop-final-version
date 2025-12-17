6db9066dccff84cd1a9453085420d85131be4b15
DawidTwardy <150292727+DawidTwardy@users.noreply.github.com>
2025-12-17 07:16:25 +0100

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,9 @@
-// prisma.config.ts
 import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
-  // Wskazujemy gdzie leży schemat
   schema: "prisma/schema.prisma",
-  // Konfiguracja bazy danych (pobiera adres z Vercel ENV)
   datasource: {
     provider: "postgresql",
-    url: process.env.DATABASE_URL, 
+    url: process.env.DATABASE_URL ?? "",
   },
 });
